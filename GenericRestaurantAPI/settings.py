09dbfe8ab8eb8eadee44b352f56ffc004ef36274
altairObando <pxnfilo@gmail.com>
2025-05-25 19:18:22 -0600

    refactor(settings): update default language and timezone to match target region
    Change LANGUAGE_CODE to 'es-mx' and TIME_ZONE to 'America/Guatemala' to better serve the primary user base in Latin America

--- GenericRestaurantAPI/settings.py
+++ GenericRestaurantAPI/settings.py
@@ -129,9 +129,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guatemala'
 
 USE_I18N = True
 
